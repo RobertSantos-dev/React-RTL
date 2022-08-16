f43e736f3ea8f4bca60d86b630870f6e0d4ec417
Robert Santos <robertdosantos09@gmail.com>
2022-08-16 20:00:56 -0300

    Resolução do 3° requisito.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes no componente App.js', () => {
+  test('Teste para mostra uma menssagem de erro caso um array vazio for passado', () => {
+    const arr = [];
+    renderWithRouter(<FavoritePokemons pokemons={ arr } />);
+
+    const mensagem = screen.getByText(/No favorite pokemon found/i);
+    expect(mensagem).toBeInTheDocument();
+  });
+
+  test('Teste para mostra uma caso um array for passado', () => {
+    const arr = [
+      { averageWeight: { value: '87', measurementUnit: 'kg' },
+        id: 1,
+        image: '../logo.svg',
+        name: 'Robert Santos',
+        type: 'Lindo e maravilhoso',
+      },
+    ];
+    renderWithRouter(<FavoritePokemons pokemons={ arr } />);
+
+    const { value, measurementUnit } = arr[0].averageWeight;
+    const frases = [
+      `Average weight: ${value} ${measurementUnit}`,
+      `${arr[0].name}`,
+      `${arr[0].type}`,
+    ];
+    frases.forEach((e) => {
+      const personagem = screen.getByText(e);
+      expect(personagem).toBeInTheDocument();
+    });
+  });
+});
